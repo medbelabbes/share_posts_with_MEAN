e716080b4818be80206fadc0cca6610593dd2364
medbelabbes <m.belabbes@esi-sba.dz>
2019-08-04 01:34:09 +0100

    Adding mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://medbelabes:MALdb4nugRJ87H0T@cluster0-nyynt.mongodb.net/mean-app?retryWrites=true&w=majority')
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,34 +31,22 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully!'
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fadf124211',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: 'qsdqsd',
-      title: 'Second server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: 'qsd321',
-      title: 'Third server-side post',
-      content: 'This is coming from the server'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched succesfylly!',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched succesfylly!',
+      posts: documents
+    });
   });
 });
 
